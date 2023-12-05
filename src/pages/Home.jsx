4e47c71759b9dc2846fb285c4de68fcc3d936423
Mikaelajoelsen <111288566+Mikaelajoelsen@users.noreply.github.com>
2023-12-05 21:00:53 +0100

    changed bg homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,20 +5,25 @@ export default function HomePage() {
     <section>
       <div className="flex flex-col mb-12 text-white bg-black md:flex-row">
         <div className="md:w-1/2">
-          <video className="w-full md:w-full" autoPlay loop muted>
+          <video
+            className="w-full bg-transparent md:w-full backdrop-blur-sm"
+            autoPlay
+            loop
+            muted
+          >
             <source src="./images/farge.mp4" type="video/mp4" />
           </video>
         </div>
-        <div className="p-4 md:w-1/2">
-          <h1 className="flex justify-center mb-2 text-2xl font-bold">
+        <div className="p-4 mt-12 md:w-1/2">
+          <h1 className="flex justify-center mb-2 text-3xl font-thin">
             WELCOME TO THIS AUCTION SITE
           </h1>
           <p className="flex justify-center mb-4 text-base">
             Explore a world of exciting auctions and find unique items just for
             you.
           </p>
-          <button className="flex px-4 py-2 mx-auto text-white border border-white rounded-full bg-inherit">
-            Get Started
+          <button className="flex px-4 py-2 mx-auto font-bold text-white border border-white rounded-full bg-inherit">
+            SIGN UP
           </button>
         </div>
       </div>
